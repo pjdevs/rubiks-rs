57a74d34153679a832681159261691e808ea6543
Pierre-Jean MOREL <pj33720@gmail.com>
2025-09-05 10:53:28 +0200

    feat: added dake daily sramble

--- rubiks-api/src/main.rs
+++ rubiks-api/src/main.rs
@@ -1,8 +1,9 @@
-use axum::Json;
+use axum::{Json, Router};
 use axum::http::StatusCode;
 use axum::extract::Query;
-use axum::Router;
 use axum::routing::get;
+use rand::seq::IndexedRandom;
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
 use std::collections::HashMap;
 use rubiks::cube::{Cube, CubeMove};
 
@@ -11,15 +12,23 @@ async fn main() {
     // build our application with a single route
     let app = Router::new()
         .route("/daily/scramble", get(get_daily_scramble))
-        .route("/cube/solved", get(get_cube_is_solved));
+        .route("/cube/solved", get(get_cube_is_solved))
+        .layer(CorsLayer::permissive())
+        .layer(TraceLayer::new_for_http());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
+static SCRAMBLES: [&str; 3] = [
+    "B2 D2 F' D2 R' D B2 U' R F' R2 F' U2 R2 F' R2 B2 U2 R2 B",
+    "B D R2 F U' F' U R F2 R2 B2 R' F2 L' U2 L2 F2 B",
+    "D' B D R2 F2 D2 R F2 R' B2 L' D2 R2 D2 F U2 R B' U L F2",
+];
+
 async fn get_daily_scramble() -> &'static str {
-    "B2 D2 F' D2 R' D B2 U' R F' R2 F' U2 R2 F' R2 B2 U2 R2 B"
+    SCRAMBLES.choose(&mut rand::rng()).unwrap_or(&SCRAMBLES[0])
 }
 
 // TODO Make a scramble extractor
